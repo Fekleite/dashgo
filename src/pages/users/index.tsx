09ff42fa7a0aa2823916d7b0938075407f1a47ee
Fekleite <fernandacarolineleite360@gmail.com>
2021-04-02 14:58:50 -0300

    feat: create pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Checkbox, Tbody
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
@@ -113,6 +114,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
 
